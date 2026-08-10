5a817cd3520b33ddfe24afc05dbab172b8af9fe4
chaithra alva <alvachaithra896@gmail.com>
2026-08-10 11:30:23 +0530

    Update preecedence.java

--- preecedence.java
+++ preecedence.java
@@ -10,3 +10,14 @@ class Main {
 }
 
 //OUTPUT 100
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        int a=10;
+        int res=a++ + a + ++a + a++;
+        System.out.println(res);
+ }
+}
+//OUTPUT 45
