0cd4c8486d97e4be0f31a34e9d1308ff2111a4e6
chaithra alva <alvachaithra896@gmail.com>
2026-08-10 12:26:09 +0530

    Create 427-a.java

---
+++ 427-a.java
@@ -0,0 +1,23 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int untreated=0,availablePolice=0;
+        int event;
+        for(int i=1;i<=n;i++){
+            event=scan.nextInt();
+            if(event > 0)
+            availablePolice+=event;
+            else{
+                if(availablePolice>0)
+                availablePolice--;
+                else
+                untreated++;
+            }
+        }
+        System.out.println(untreated);
+    }
+}
