7e6650fae97a6d69cabedad9f82f0cefd4abfb34
chaithra alva <alvachaithra896@gmail.com>
2026-08-10 13:00:25 +0530

    Create 266-B.java

---
+++ 266-B.java
@@ -0,0 +1,20 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int t=scan.nextInt();
+        String s=scan.next();
+        while(t-->0){
+            for(int i=0;i<s.length()-1;i++){
+                if(s.charAt(i)=='B' && s.charAt(i+1)=='G'){
+                    s=s.substring(0,i)+"GB"+s.substring(i+2);
+                    i++;
+                }
+            }
+        }
+        System.out.println(s);
+}
+}
